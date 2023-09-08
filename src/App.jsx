78d7dcf6fa60b0b1eacf66cd98a79fd3d8f60da2
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-08 22:09:38 +0700

    feat: set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
   return (
     <>
-      <h1>Eventopia</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
