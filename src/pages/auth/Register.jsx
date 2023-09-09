ec45ea627864a04fd0f5c318c2813f721db81f47
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-10 00:23:51 +0700

    feat(register): create, style, and apply formik in register component

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,8 +1,20 @@
+import { Stack, Heading } from '@chakra-ui/react';
+import RegisterForm from '../../components/RegisterForm';
+
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Stack
+      w='100%'
+      h='100vh'
+      justifyContent='center'
+      alignItems='center'
+      bg='gray.50'
+    >
+      <Heading as='h1' fontSize='2xl' mb='5' color='gray.700' textAlign='left'>
+        Eventopia.
+      </Heading>
+      <RegisterForm />
+    </Stack>
   );
 }
 
