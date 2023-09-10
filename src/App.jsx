81728d92c96396e5caf3c705c28a9375c5341948
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-10 10:48:18 +0700

    feat(event): create event detail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
 import Dashboard from './pages/Dashboard';
+import Event from './pages/Event';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/event/' element={<Event />} />
       </Routes>
     </>
   );
